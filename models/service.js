e5dbbd762e2df2142f64f3e68c46e17444fc8a63
Hùynh Như Bảo Nhân <baonhannice@gmail.com>
2024-05-16 23:44:48 +0700

    #4.1  API-CRUD

--- models/service.js
+++ models/service.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const data = require('../db.json');
+
 function writeData(data) {
     const filePath = path.join(__dirname, '../db.json');
     fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf-8');
@@ -13,18 +14,20 @@ function getAll() {
 function getOne(id) {
     const index =  data.products.findIndex(product => product.id === id);
     if(index === -1) {
-        return null;
+        return id;
     }
     return data.products[index];
 }
 
 function create(product) {
-    let genid = data.products.length;
-    product.id = genid + 1;
+    let maxID = data.products[data.products.length - 1].id;
+    const newId = parseInt(maxID) + 1;
+    product.id = newId.toString();
     data.products.push(product);
     writeData(data);
     return product;
 }
+
 
 function update(id, product) {
     let index = data.products.findIndex(product => product.id === id);
