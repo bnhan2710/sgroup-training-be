0cb3d65442b06f56e7f6dd2e38c0c2d74a13f3f9
Hùynh Như Bảo Nhân <baonhannice@gmail.com>
2024-05-21 19:41:55 +0700

    validate input

--- routes/api.route.js
+++ routes/api.route.js
@@ -3,9 +3,21 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productsControllers');
 
+const Validate = (req,res,next) => {
+    const {price,quantity} = req.body;
+    if(!price || price<= 0){
+        res.status(400).json('Price must be greater than 0');
+    }
+    if(!quantity || quantity<= 0){
+        res.status(400).json('Quantity must be greater than 0');
+    }
+    next();
+}
+ 
+
 router.get('/users', productController.getAll);
-router.get('/users/:id', productController.getOne);
-router.post('/users', productController.create);
+router.get('/users/:id',productController.getOne);
+router.post('/users', Validate, productController.create);
 router.put('/users/:id', productController.update);
 router.delete('/users/:id', productController.remove);
 
